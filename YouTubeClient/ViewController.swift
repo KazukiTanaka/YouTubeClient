96233f64294283ffc25863c568048a43df951d41
KazukiTanaka <kazuki.tanaka.dev.uxd@gmail.com>
2018-08-22 19:16:44 +0900

    Initial Commit

---
+++ YouTubeClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YouTubeClient
+//
+//  Created by Kazuki Tanaka on 2018/08/22.
+//  Copyright © 2018年 Kazuki Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
